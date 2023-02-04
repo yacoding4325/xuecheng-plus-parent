8124d063602547448d86b7b1d1bf73bb7185e7de
yxym0203 <1500081802@qq.com>
2023-02-04 13:59:05 +0800

    创建工程模块，利用代码生成器 完成数据模块化搭建

---
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/content/service/TeachplanMediaService.java
@@ -0,0 +1,16 @@
+package com.xuecheng.content.service;
+
+import com.xuecheng.content.model.po.TeachplanMedia;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author itcast
+ * @since 2022-10-07
+ */
+public interface TeachplanMediaService extends IService<TeachplanMedia> {
+
+}
