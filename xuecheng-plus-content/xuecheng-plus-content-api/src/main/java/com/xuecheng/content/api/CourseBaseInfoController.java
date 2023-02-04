654187807d45d6b311e389be875882f61e3d418d
yxym0203 <1500081802@qq.com>
2023-02-04 20:58:35 +0800

    在线接口文档的生成工具 swagger使用

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -12,14 +12,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Api(value = "课程管理接口",tags = "课程管理接口")
 @RestController
 public class CourseBaseInfoController {
 
+//    @ApiOperation("课程查询接口")
+//    @PostMapping("/course/list")
+//  public PageResult<CourseBase> list(PageParams params, @RequestBody QueryCourseParamsDto queryCourseParamsDto){
+//     return  null;
+//    }
+
     @ApiOperation("课程查询接口")
     @PostMapping("/course/list")
-  public PageResult<CourseBase> list(PageParams params, @RequestBody QueryCourseParamsDto queryCourseParamsDto){
-     return  null;
+    public PageResult<CourseBase> list(PageParams pageParams, @RequestBody QueryCourseParamsDto queryCourseParams){
+
+        CourseBase courseBase = new CourseBase();
+        courseBase.setName("测试名称");
+        courseBase.setCreateDate(LocalDateTime.now());
+        List<CourseBase> courseBases = new ArrayList();
+        courseBases.add(courseBase);
+        PageResult pageResult = new PageResult<CourseBase>(courseBases,10,1,10);
+        return pageResult;
     }
 
 }
