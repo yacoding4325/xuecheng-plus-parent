d1ff739a6ee2130c4feaa162aa666a3a4ccece77
yxym0203 <1500081802@qq.com>
2023-02-10 19:47:27 +0800

    课程id

---
+++ xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/EditCourseDto.java
@@ -0,0 +1,15 @@
+package com.xuecheng.content.model.dto;
+
+import lombok.Data;
+
+/**
+ * @Author yaCoding
+ * @create 2023-02-10 下午 3:54
+ */
+@Data
+public class EditCourseDto extends AddCourseDto {
+
+    //课程id
+    private Long id;
+
+}
