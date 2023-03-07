de9b3165a118461b3d951a5884bb65011c8a0151
yxym0203 <1500081802@qq.com>
2023-03-07 17:06:06 +0800

    课程发布 -- 课程出版

---
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/content/service/CoursePublishService.java
@@ -0,0 +1,12 @@
+package com.xuecheng.content.service;
+
+import com.xuecheng.content.model.po.CoursePublish;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Author yaCoding
+ * @create 2023-03-07 下午 3:47
+ */
+
+public interface CoursePublishService extends IService<CoursePublish> {
+}
