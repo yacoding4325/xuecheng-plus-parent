66b86ecbbcf080f7e353cc677c4df24f29fb7ea2
yxym0203 <1500081802@qq.com>
2023-03-27 09:37:24 +0800

    定义一个响应的 模型类

--- xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageResult.java
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageResult.java
@@ -5,15 +5,12 @@ import lombok.ToString;
 
 import java.util.List;
 
-/**
- * @description TODO
- * @author Mr.M
- * @date 2022/10/7 16:17
- * @version 1.0
- */
+//定义一个响应的 模型类
+
  @Data
  @ToString
 public class PageResult<T> {
+
   // 数据列表
   private List<T> items;
 
@@ -27,10 +24,15 @@ public class PageResult<T> {
   private long pageSize;
 
   public PageResult(List<T> items, long counts, long page, long pageSize) {
+
    this.items = items;
+
    this.counts = counts;
+
    this.page = page;
+
    this.pageSize = pageSize;
+
   }
 
  }
