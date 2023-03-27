0ae7e008ec13f057a3400660f160b0f72d824cfd
yxym0203 <1500081802@qq.com>
2023-03-27 13:01:57 +0800

    单元测试

---
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/CourseBaseMapperTests.java
@@ -0,0 +1,27 @@
+package com.xuecheng.content;
+
+import com.xuecheng.content.mapper.CourseBaseMapper;
+import com.xuecheng.content.model.po.CourseBase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author yaCoding
+ * @create 2023-03-27 上午 10:54
+ */
+
+@SpringBootTest
+public class CourseBaseMapperTests {
+
+    @Autowired
+    CourseBaseMapper courseBaseMapper;
+
+    @Test
+    public void testCourseBaseMapper() {
+        CourseBase courseBase = courseBaseMapper.selectById(18);
+        Assertions.assertNotNull(courseBase);
+    }
+
+}
