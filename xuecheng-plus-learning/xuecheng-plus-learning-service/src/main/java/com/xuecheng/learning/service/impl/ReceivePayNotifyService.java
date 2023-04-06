989124514d4400991f5087229ffec24dbd3b7237
yxym0203 <1500081802@qq.com>
2023-04-06 21:51:43 +0800

    ReceivePayNotifyService 接收支付通知服务

--- xuecheng-plus-learning/xuecheng-plus-learning-service/src/main/java/com/xuecheng/learning/service/impl/ReceivePayNotifyService.java
+++ xuecheng-plus-learning/xuecheng-plus-learning-service/src/main/java/com/xuecheng/learning/service/impl/ReceivePayNotifyService.java
@@ -1,84 +1,54 @@
 package com.xuecheng.learning.service.impl;
 
 import com.alibaba.fastjson.JSON;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.xuecheng.base.execption.XueChengPlusException;
 import com.xuecheng.learning.config.PayNotifyConfig;
-import com.xuecheng.learning.mapper.XcChooseCourseMapper;
-import com.xuecheng.learning.model.po.XcChooseCourse;
 import com.xuecheng.learning.service.MyCourseTablesService;
 import com.xuecheng.messagesdk.model.po.MqMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 /**
  * @Author yaCoding
  * @create 2023-02-20 下午 6:16
  */
 @Slf4j
-@Component
+@Service
+//接收支付通知服务
 public class ReceivePayNotifyService {
-
-    @Autowired
-    XcChooseCourseMapper chooseCourseMapper;
 
     @Autowired
     MyCourseTablesService myCourseTablesService;
 
-    @Autowired
+    @RabbitListener(queues = PayNotifyConfig.PAYNOTIFY_QUEUE)
+    public void receive(Message message) {
 
-    RabbitTemplate rabbitTemplate;
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-    //监听支付结果通知队列
-    @RabbitListener(queues = {PayNotifyConfig.PAYNOTIFY_QUEUE})
-    public void receive(String message) {
-
-        //解析消息
-        MqMessage mqMessage = JSON.parseObject(message, MqMessage.class);
-        //判断该消息是否自己的消息
-        String messageType = mqMessage.getMessageType();
-        //记录了订单类型
-        String businessKey2 = mqMessage.getBusinessKey2();
-        //只处理支付结果通知的消息,并且是学生购买课程的订单的消息
-        if (PayNotifyConfig.MESSAGE_TYPE.equals(messageType) && "60201".equals(businessKey2)) {
-            //根据选课id查询选课表的记录
-            String businessKey1 = mqMessage.getBusinessKey1();
-            XcChooseCourse xcChooseCourse = chooseCourseMapper.selectById(businessKey1);
-            if (xcChooseCourse == null) {
-                log.info("收到支付结果通知,查询不到选课记录,businessKey1:{}", businessKey1);
-                return;
+        //解析出消息
+        byte[] body = message.getBody();
+        String jsonString = new String(body);
+        //转成对象
+        MqMessage mqMessage = JSON.parseObject(jsonString, MqMessage.class);
+        //解析消息的内容
+        //选课id
+        String chooseCourseId = mqMessage.getBusinessKey1();
+        //订单类型
+        String orderType = mqMessage.getBusinessKey2();
+        //学习中心服务只要购买课程类的支付订单的结果
+        if (orderType.equals("60201")) {
+            //根据消息内容，更新选课记录、向我的课程表插入记录
+            boolean b = myCourseTablesService.saveChooseCourseSuccess(chooseCourseId);
+            if (!b) {
+                XueChengPlusException.cast("保证选课记录状态失败");
             }
-
-
-            XcChooseCourse xcChooseCourse_u = new XcChooseCourse();
-            xcChooseCourse_u.setStatus("701001");//选课成功
-            chooseCourseMapper.update(xcChooseCourse_u, new LambdaQueryWrapper<XcChooseCourse>().eq(XcChooseCourse::getId, businessKey1));
-
-            //查询最新的选课记录
-            xcChooseCourse = chooseCourseMapper.selectById(businessKey1);
-            //向我的课程表添加记录
-            myCourseTablesService.addCourseTabls(xcChooseCourse);
-
-            //发送回复
-            send(mqMessage);
-
-
         }
     }
-
-    /**
-     * @description 回复消息
-     * @param message  回复消息
-     * @return void
-     */
-    public void send(MqMessage message){
-        //转json
-        String msg = JSON.toJSONString(message);
-        // 发送消息
-        rabbitTemplate.convertAndSend(PayNotifyConfig.PAYNOTIFY_REPLY_QUEUE, msg);
-        log.debug("学习中心服务向订单服务回复消息:{}",message);
-    }
-
 }
